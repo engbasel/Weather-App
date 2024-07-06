da5d91b2b8570b3bdd8d3791b90d462bb4c7fadd
Basel Embaby <engbasel2025@gmail.com>
2024-07-06 16:55:56 +0300

    "Refactored HomeScreen and DescribeWeatherScreen widgets, updated layout and styling."

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,3 +1,172 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/Modules/Models.dart';
+// import 'package:flutter_application_1/providers/weatherProvider.dart';
+// import 'package:flutter_application_1/screens/SearchPage.dart';
+// import 'package:provider/provider.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   void UI_update() {
+//     setState(() {});
+//   }
+
+//   WeatherModel? weatherData;
+//   @override
+//   Widget build(BuildContext context) {
+//     weatherData = Provider.of<weatherProviderclass>(context).weatherData;
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         actions: [
+//           IconButton(
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: ((context) {
+//                     return SearchPage(
+//                       UI_update: UI_update,
+//                     );
+//                   }),
+//                 ),
+//               );
+//             },
+//             icon: Icon(
+//               Icons.search,
+//             ),
+//           ),
+//         ],
+//         title: Text(
+//           'Home',
+//         ),
+//       ),
+//       body: weatherData == null
+//           ? Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Spacer(
+//                   flex: 2,
+//                 ),
+//                 Center(
+//                   child: Text(
+//                     'No Citys Avilabel Now ',
+//                     style: TextStyle(
+//                       fontSize: 15.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 Spacer(
+//                   flex: 1,
+//                 ),
+//                 Center(
+//                   child: Text(
+//                     'Scrch about Youre city Now',
+//                     style: TextStyle(
+//                       fontSize: 15.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 Spacer(
+//                   flex: 2,
+//                 ),
+//               ],
+//             )
+//           // start of new screen
+//           // --------------------------
+//           : Container(
+//               decoration: BoxDecoration(
+//                   gradient: LinearGradient(
+//                       begin: Alignment.topCenter,
+//                       end: Alignment.bottomCenter,
+//                       colors: [
+//                     weatherData!.backGrongThems()[900]!,
+//                     weatherData!.backGrongThems()[400]!,
+//                     weatherData!.backGrongThems()[300]!,
+//                     weatherData!.backGrongThems()[200]!,
+//                     weatherData!.backGrongThems()[100]!,
+//                     weatherData!.backGrongThems()[100]!,
+//                     weatherData!.backGrongThems()[100]!,
+//                     weatherData!.backGrongThems(),
+//                   ])),
+//               // color: weatherData!.backGrongThems(),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: [
+//                   Text(
+//                     weatherData!.cityName ?? ' please Enter City Name Again',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 40,
+//                     ),
+//                   ),
+//                   Text(
+//                     weatherData!.date,
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 20,
+//                     ),
+//                   ),
+//                   SingleChildScrollView(
+//                     scrollDirection: Axis.horizontal,
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         Image.asset(
+//                           weatherData!.ImageIcon(),
+//                         ),
+//                         Text(
+//                           'Average temp is ${weatherData!.avgTemp.toInt().toString()}',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 20,
+//                           ),
+//                         ),
+//                         Column(
+//                           children: [
+//                             // 'Min temp 20'
+//                             Text(
+//                               'Max Temp ${weatherData!.maxTemp.toInt()}',
+//                               style: TextStyle(
+//                                 fontWeight: FontWeight.bold,
+//                                 fontSize: 20,
+//                               ),
+//                             ),
+//                             // 'max temp 20'
+//                             Text(
+//                               'Min Temp ${weatherData!.minTemp.toInt()}',
+//                               style: TextStyle(
+//                                 fontWeight: FontWeight.bold,
+//                                 fontSize: 20,
+//                               ),
+//                             ),
+//                           ],
+//                         )
+//                       ],
+//                     ),
+//                   ),
+//                   Text(
+//                     // weather state
+//                     weatherData!.weatherState ??
+//                         'Sorry Cant Fint Weather State Now try a gain ',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 20,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Modules/Models.dart';
 import 'package:flutter_application_1/providers/weatherProvider.dart';
@@ -15,6 +184,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   WeatherModel? weatherData;
+
   @override
   Widget build(BuildContext context) {
     weatherData = Provider.of<weatherProviderclass>(context).weatherData;
@@ -46,42 +216,40 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: weatherData == null
-          ? Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Spacer(
-                  flex: 2,
-                ),
-                Center(
-                  child: Text(
-                    'No Citys Avilabel Now ',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No Cities Available Now',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                ),
-                Spacer(
-                  flex: 1,
-                ),
-                Center(
-                  child: Text(
-                    'Scrch about Youre city Now',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
+                    SizedBox(height: 20),
+                    Text(
+                      'Search for your city now',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                  ),
+                  ],
                 ),
-                Spacer(
-                  flex: 2,
-                ),
-              ],
+              ),
             )
-          // start of new screen
-          // --------------------------
           : Container(
               decoration: BoxDecoration(
+                  // gradient: LinearGradient(
+                  //   begin: Alignment.topCenter,
+                  //   end: Alignment.bottomCenter,
+                  //   colors: weatherData!.backGrongThems(),
+                  // ),
                   gradient: LinearGradient(
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
@@ -95,69 +263,72 @@ class _HomeScreenState extends State<HomeScreen> {
                     weatherData!.backGrongThems()[100]!,
                     weatherData!.backGrongThems(),
                   ])),
-              // color: weatherData!.backGrongThems(),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    weatherData!.cityName ?? ' please Enter City Name Again',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40,
-                    ),
-                  ),
-                  Text(
-                    weatherData!.date,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset(
-                        weatherData!.ImageIcon(),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      weatherData!.cityName ?? 'Please Enter City Name Again',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 36,
                       ),
-                      Text(
-                        'Average temp is ${weatherData!.avgTemp.toInt().toString()}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          // 'Min temp 20'
-                          Text(
-                            'Max Temp ${weatherData!.maxTemp.toInt()}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          // 'max temp 20'
-                          Text(
-                            'Min Temp ${weatherData!.minTemp.toInt()}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  Text(
-                    // weather state
-                    weatherData!.weatherState ??
-                        'Sorry Cant Fint Weather State Now try a gain ',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                ],
+                    Text(
+                      weatherData!.date,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Image.asset(
+                      weatherData!.ImageIcon(),
+                      height: 100,
+                      width: 100,
+                    ),
+                    Text(
+                      'Average Temperature: ${weatherData!.avgTemp.toInt()}°',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Max Temp: ${weatherData!.maxTemp.toInt()}°',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              'Min Temp: ${weatherData!.minTemp.toInt()}°',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Text(
+                      weatherData!.weatherState ??
+                          'Sorry, cannot find weather state now. Try again.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ),
             ),
     );
--- lib/screens/discribeweather.dart
+++ lib/screens/discribeweather.dart
@@ -1,90 +1,177 @@
+// import 'package:flutter/material.dart';
+
+// class discribe_weatherScreen extends StatefulWidget {
+//   const discribe_weatherScreen({Key? key}) : super(key: key);
+//   @override
+//   State<discribe_weatherScreen> createState() => _discribe_weatherScreenState();
+// }
+
+// class _discribe_weatherScreenState extends State<discribe_weatherScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.orange,
+//       // appBar: AppBar(
+//       //   title: Text(
+//       //     'Home ',
+//       //   ),
+//       // ),
+//       body: Column(
+//         children: [
+//           Spacer(
+//             flex: 1,
+//           ),
+//           Text(
+//             'Cairo',
+//             style: TextStyle(
+//               color: Colors.black,
+//               fontSize: 30.0,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//           Text(
+//             'Last update 10:45',
+//             style: TextStyle(
+//               fontSize: 15,
+//             ),
+//           ),
+//           Spacer(
+//             flex: 1,
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               Icon(
+//                 Icons.sunny,
+//                 color: Colors.yellow,
+//                 size: 40.0,
+//               ),
+//               Text(
+//                 '30.0',
+//                 style: TextStyle(
+//                   fontSize: 20.0,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               Column(
+//                 children: [
+//                   Text(
+//                     'min temp 10',
+//                     style: TextStyle(
+//                       fontSize: 15.0,
+//                     ),
+//                   ),
+//                   Text(
+//                     'Maxtemp 10',
+//                     style: TextStyle(
+//                       fontSize: 15.0,
+//                     ),
+//                   )
+//                 ],
+//               )
+//             ],
+//           ),
+//           Spacer(
+//             flex: 1,
+//           ),
+//           Text(
+//             'Clear',
+//             style: TextStyle(
+//               fontSize: 30.0,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//           Spacer(
+//             flex: 3,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-class discribe_weatherScreen extends StatefulWidget {
-  const discribe_weatherScreen({Key? key}) : super(key: key);
+class DescribeWeatherScreen extends StatefulWidget {
+  const DescribeWeatherScreen({Key? key}) : super(key: key);
+
   @override
-  State<discribe_weatherScreen> createState() => _discribe_weatherScreenState();
+  State<DescribeWeatherScreen> createState() => _DescribeWeatherScreenState();
 }
 
-class _discribe_weatherScreenState extends State<discribe_weatherScreen> {
+class _DescribeWeatherScreenState extends State<DescribeWeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange,
-      // appBar: AppBar(
-      //   title: Text(
-      //     'Home ',
-      //   ),
-      // ),
-      body: Column(
-        children: [
-          Spacer(
-            flex: 1,
-          ),
-          Text(
-            'Cairo',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'Last update 10:45',
-            style: TextStyle(
-              fontSize: 15,
-            ),
-          ),
-          Spacer(
-            flex: 1,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(
-                Icons.sunny,
-                color: Colors.yellow,
-                size: 40.0,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Cairo',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                '30.0',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'Last update: 10:45',
+              style: TextStyle(
+                fontSize: 16.0,
               ),
-              Column(
+            ),
+            SizedBox(height: 32.0),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(
-                    'min temp 10',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
+                  Icon(
+                    Icons.sunny,
+                    color: Colors.yellow,
+                    size: 40.0,
                   ),
                   Text(
-                    'Maxtemp 10',
+                    '30.0°C',
                     style: TextStyle(
-                      fontSize: 15.0,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Min Temp: 10°C',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        'Max Temp: 10°C',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
-          ),
-          Spacer(
-            flex: 1,
-          ),
-          Text(
-            'Clear',
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Spacer(
-            flex: 3,
-          ),
-        ],
+            SizedBox(height: 32.0),
+            Text(
+              'Clear',
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
